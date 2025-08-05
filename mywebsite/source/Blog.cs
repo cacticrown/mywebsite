f2f41814096e10106aba164d7dc3ac4f161c5eb9
cacticrown <nathan.kichler@outlook.com>
2025-08-05 13:56:24 +0200

    fixed deploy.yml wrong publish dir and fixed warning

--- mywebsite/source/Blog.cs
+++ mywebsite/source/Blog.cs
@@ -2,7 +2,7 @@
 
 public class Blog
 {
-    public string UnmodifiedName { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string UnmodifiedName { get; set; } = String.Empty;
+    public string Name { get; set; } = String.Empty;
+    public string Description { get; set; } = String.Empty;
 }
